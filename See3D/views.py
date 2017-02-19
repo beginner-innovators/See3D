4a84b0b58990a57f22e39053af7d0523be60a87a
Davis <davis.defoor@gmail.com>
2017-02-19 02:02:03 -0500

    Add very basic functionality to main page.

--- See3D/views.py
+++ See3D/views.py
@@ -1,8 +1,25 @@
 from flask import render_template
 from See3D import app
 
+requests = [{'user': 'Barbara', 'body': 'Toy Train'},
+            {'user': 'Miguel', 'body': 'Globe'},
+            {'user': 'Jennifer', 'body': 'Photograph'}]
+test_user = {"username": "Billy", "requests": requests}
+
 @app.route('/')
-@app.route('/index')
+@app.route('/index/')
 def index():
-    user = {'nickname': "Billy"}    # fake user
     return render_template('index.html')
+
+@app.route('/profile/')
+@app.route('/profile/<username>/')
+def profile(username=""):
+    if username.lower() == test_user["username"].lower():
+        user = test_user
+    else:
+        user = None
+
+    return render_template('profile.html',
+                           title="Profile",
+                           user=user)
+
