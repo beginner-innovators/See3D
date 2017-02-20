d8eca1f61c073234e685b56b6cbac6507717d032
beginner-innovators <innovationclub17@gmail.com>
2017-02-20 02:23:55 -0500

    Update run_https.py

--- run_https.py
+++ run_https.py
@@ -3,6 +3,6 @@ import ssl
 from See3D import app
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-context.load_cert_chain('../stunnel.cert', '../stunnel.key')
+context.load_cert_chain('<https certification>', '<https key>')
 
 app.run(host='127.0.0.1', port=5000, debug=True, ssl_context=context)
