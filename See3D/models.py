83c5272a36dcc4d5ea9e4a24f3bc97fef9cd4f73
Davis <davis.defoor@gmail.com>
2017-02-24 03:11:18 -0500

    Add working gallery page.

--- See3D/models.py
+++ See3D/models.py
@@ -4,17 +4,14 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64))
+
     email = db.Column(db.String(254))
     creator = db.Column(db.Boolean, default=False)
-
     sub = db.Column(db.Text)        # Taken from 'sub' field of Google ID Token
 
-    def __init__(self, username, email, creator, sub):
-        self.username = username
+    def __init__(self, email, creator, sub):
         self.email = email
         self.creator = creator
-
         self.sub = sub
 
     def __repr__(self):
--- See3D/views.py
+++ See3D/views.py
@@ -1,11 +1,12 @@
 import json
 
 import httplib2
-from flask import redirect, render_template, request, session, url_for
-from flask_login import login_required
+import flask
+from flask_login import login_required, login_user, logout_user
 from oauth2client import client
 
-from See3D import app, db, login_manager, models
+from See3D import app, db, login_manager
+from See3D.models import User
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -14,42 +15,84 @@ def load_user(user_id):
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template('index.html')
+    return flask.render_template('index.html')
 
 @app.route('/about/')
 @login_required
 def about():
-    return render_template('about.html', title="About")
+    return flask.render_template('about.html', title="About")
 
 @app.route('/gallery/')
-@login_required
 def gallery():
-    return render_template('gallery.html', title="Gallery")
+    requests = []
+
+    requests.append({
+        "id": 5738493,
+        "title": "Castle",
+        "description": "A small, basic castle would do just fine.",
+        "user": {"email":"jbr101@gmail.com"}
+    })
+
+    requests.append({
+        "id": 2023423,
+        "title": "House",
+        "description": "Make it the largest mansion you've ever felt.",
+        "user": {"email":"aubrymanson@yahoo.com"}
+    })
+
+    return flask.render_template('gallery.html', title="Gallery", requests=requests)
+
+@app.route('/requests/<request_id>/')
+def request(request_id):
+    return "Request {}".format(request_id)
+
+@app.route('/submit/')
+@login_required
+def submit():
+    return "This is where users will submit requests."
 
 @app.route('/profile/')
 @login_required
 def profile():
-    return render_template('profile.html', title="Profile")
+    return flask.render_template('profile.html', title="Profile")
 
 @app.route('/oauth2callback')
 def oauth2callback():
     flow = client.flow_from_clientsecrets(
         '../client_secrets.json',
-        scope='https://www.googleapis.com/auth/userinfo.profile',
-        redirect_uri=url_for('oauth2callback', _external=True),
+        scope='email profile',
+        redirect_uri=flask.url_for('oauth2callback', _external=True),
     )
     flow.params['include_granted_scopes'] = "true"
 
     # Redirect user to Google sign-in.
-    if 'code' not in request.args:
+    if 'code' not in flask.request.args:
         auth_uri = flow.step1_get_authorize_url()
-        return redirect(auth_uri)
+        return flask.redirect(auth_uri)
 
     # Upon callback, obtain the user's credentials.
     else:
-        auth_code = request.args.get('code')
-        credentials = flow.step2_exchange(auth_code).to_json()
+        auth_code = flask.request.args.get('code')
+        credentials = flow.step2_exchange(auth_code)
 
-        sub = credentials["id_token"]["sub"]        # Primary identification of Google account
-        user = models.User.query.filter_by(sub=sub).first()
+        user_info = json.loads(credentials.to_json())
 
+        sub = user_info["id_token"]["sub"]        # Primary identification of Google account
+        user = User.query.filter_by(sub=sub).first()
+
+        # Add user to database if not already there.
+        if not user:
+            email = user_info["id_token"]["email"]
+            user = User(email=email, creator=False, sub=sub)
+
+            db.session.add(user)
+            db.session.commit()
+
+        login_user(user, remember=True)
+        return flask.redirect(flask.url_for('index'))
+
+@app.route('/logout/')
+@login_required
+def logout():
+    logout_user()
+    return flask.redirect('index')
