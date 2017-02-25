f61aa3b9552c4e04fbbb797ab6cc905a118c4a8e
daviswithanS <davis.defoor@gmail.com>
2017-02-25 11:59:50 -0500

    Edit about text and add production mode.

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+import sys
 from See3D import app
-app.run(host='127.0.0.1', port=5000, debug=True)
+
+if '--debug=True' in sys.argv:
+    app.run(host='127.0.0.1', port=5000, debug=True)
+else:
+    print(' * Running on production. Use the flag "--debug=True" for debug mode.')
+    app.run(host='127.0.0.1', port=5000, debug=False)
