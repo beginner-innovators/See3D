bf95f1ae8fbe6baab0b3d3d7d20abae05cc13e18
daviswithanS <davis.defoor@gmail.com>
2017-02-27 19:36:14 -0500

    Change production mode activation to '--production'

--- run.py
+++ run.py
@@ -2,8 +2,7 @@
 import sys
 from See3D import app
 
-if '--debug=True' in sys.argv:
-    app.run(host='127.0.0.1', port=5000, debug=True)
-else:
-    print(' * Running on production. Use the flag "--debug=True" for debug mode.')
+if "--production" in sys.argv:
     app.run(host='127.0.0.1', port=5000, debug=False)
+else:
+    app.run(host='127.0.0.1', port=5000, debug=True)
