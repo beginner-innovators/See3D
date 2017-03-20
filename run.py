c625ae335292a64c143edfe5d67efab8d5b4a708
Davis <daviswithanS@users.noreply.github.com>
2017-03-20 10:50:38 -0400

    Change production flag.
    "--production" => "--prod=True"

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import sys
 from See3D import app
 
-if "--production" in sys.argv:
+if "--prod=True" in sys.argv:
     app.run(host='127.0.0.1', port=5000, debug=False)
 else:
     app.run(host='127.0.0.1', port=5000, debug=True)
