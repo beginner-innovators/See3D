8cd47f84628bd5d520ddfcfcc4f5d695a85880fb
beginner-innovators <innovationclub17@gmail.com>
2017-04-10 10:33:45 -0400

    Rename database to "prod.db".

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from secret_key import key
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, "test.db")
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, "prod.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY = key
